366907b5428daec9b0b8b1fd6dc61889a745567b
Rizky Ramadhan <ramadhanky68@gmail.com>
2020-10-02 14:09:00 +0700

    Added some classes

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+    Class About{
+        public function index($nama = "Rizky", $pekerjaan = "Game dev"){
+            echo "Halo nama saya $nama, Saya seorang $pekerjaan";
+        }
+        public function page(){
+            echo 'About/page';
+        }
+    }
+
+?>
